9cfb56f8a9d406423984859c4454593bf4227eb1
Michael Jones <michaeljones98@hotmail.co.uk>
2022-05-17 12:19:56 +0100

    Add Patch article by id endpoint

--- Model/articles.model.js
+++ Model/articles.model.js
@@ -4,8 +4,29 @@ exports.selectArticleById = (article_id) => {
 
     return db.query('SELECT * FROM articles WHERE article_id = $1', [article_id]).then(results => {
         if(results.rows.length === 0){
-                return Promise.reject({status: 404, msg: 'article doesnt exist'})
+            return Promise.reject({status: 404, msg: 'article doesnt exist'})
         }
+        return results.rows[0]
+    })
+}
+
+exports.updateArticleById = (article_id, inc_votes) => {
+    
+    return db.query('SELECT * FROM articles WHERE article_id = $1', [article_id]).then(results => {
+
+        if(results.rows.length === 0){
+            return Promise.reject({status: 404, msg: 'article doesnt exist'})
+        } 
+
+        const checkVotes = Math.sqrt(Math.pow(inc_votes, 2))
+        if(checkVotes > results.rows[0].votes) {
+            return Promise.reject({status: 400, msg: 'bad request'})
+        }
+
+        const value = results.rows[0].votes + inc_votes
+        
+        return db.query('UPDATE articles SET votes = $1 WHERE article_id = $2 RETURNING *', [value, article_id])
+    }).then(results => {
         return results.rows[0]
     })
 }
--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const {getTopics} = require('./Controller/topics.controller')
-const {getArticleById} = require('./Controller/articles.contoller')
+const {getArticleById, patchArticleById} = require('./Controller/articles.contoller')
 
 const app = express()
+app.use(express.json())
 
 app.get('/api/topics', getTopics)
 
 app.get('/api/articles/:article_id', getArticleById)
+app.patch('/api/articles/:article_id', patchArticleById)
 
 app.use('/*', (req, res, next) => {
     res.status(404).send({msg: 'Not found'})
@@ -14,6 +16,8 @@ app.use('/*', (req, res, next) => {
 
 app.use((err, req, res, next) => {
     if (err.code === '22P02'){
+        res.status(400).send({msg: 'bad request'})
+    } else if (err.status === 400 && err.msg === 'bad request'){
         res.status(400).send({msg: 'bad request'})
     } else {
         next(err)
