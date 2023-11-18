8ecdf9374d71a75903eba978a6d89f5814f577b2
Miike-J <michaeljones98@hotmail.co.uk>
2023-11-18 17:44:13 +0000

    Rework for clever cloud

--- db/connection.js
+++ db/connection.js
@@ -5,17 +5,14 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-if (!process.env.PGDATABASE && !process.env.DB_CONNECTION_URL) {
-  throw new Error('PGDATABASE or DB_CONNECTION_URL not set');
+const connectionString = process.env.DB_CONNECTION_URL || process.env.PGDATABASE;
+
+if (!connectionString) {
+  throw new Error('DB_CONNECTION_URL or PGDATABASE not set');
 }
-const config =
-  ENV === 'production'
-    ? {
-        connectionString: process.env.DB_CONNECTION_URL,
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      }
-    : {};
+
+const config = {
+  connectionString: connectionString,
+};
 
 module.exports = new Pool(config);
