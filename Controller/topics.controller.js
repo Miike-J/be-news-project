5565b10bb838b4618ea8042f81880a0febd87e42
Miike-J <michaeljones98@hotmail.co.uk>
2023-11-20 18:19:17 +0000

    error console log

--- Controller/topics.controller.js
+++ Controller/topics.controller.js
@@ -1,6 +1,9 @@
 const {selectTopics} = require('../Model/topics.model')
 
 exports.getTopics = (req, res) => {
+
+    console.log('>>>>>>>>>>>>>>>>>>Shmere<<<<<<<<<<<<<<<<<<<<<,')
+
     selectTopics().then(results => {
         res.status(200).send({topics: results})
     })
